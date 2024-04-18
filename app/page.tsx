60ae0cefa4ddf91a690ef10068ff43779a684e83
Victor Hugo <victorzarbesu@gmail.com>
2024-04-18 09:51:52 -0300

    testando a api

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
-import Image from "next/image";
+import { Client } from "./api/clients/interfaces";
 
-export default function Home() {
+interface Item {
+  name: string
+  address: string
+}
+
+export default async function Home() {
+  const response = await fetch('http://localhost:3000/api/clients');
+  const json = await response.json()
+
+  console.log(json)
+
   return (
     <main>
-      Conteudo
-    </main>
+    {
+    json.map((item: Client)  => {
+        return (
+          <>
+            <div className='border bg-gray-300 my-3 p-2 flex justify'>
+              <div className='upercase flex-1'>{item.name}</div>
+              <div className='flex-1 italic'>{item.address}</div>
+            </div>
+          </>
+        );
+    })}
+ </main>
   );
 }
